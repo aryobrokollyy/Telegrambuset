afb38b72959e29e7271596308d01a6cd665118b5
Aryo Brokolly <aryobrokolly92@gmail.com>
2025-05-29 21:31:55 +0700

    Update telegram_config.lua

--- telegram_config.lua
+++ telegram_config.lua
@@ -4,19 +4,19 @@ m = Map("telegram", translate("Telegram Settings"), translate("Configure Telegra
 
 s = m:section(NamedSection, "settings", "telegram", translate("Telegram Configuration"))
 
-o = s:option(Value, "group_id", translate("Group ID"))
-o.datatype = "string"
-o.placeholder = "Enter your Telegram Group ID"
-
-o = s:option(Value, "message_thread_id", translate("Message Thread ID"))
-o.datatype = "string"
-o.placeholder = "Enter your Telegram Message Thread ID"
-
 o = s:option(Value, "bot_token", translate("Bot Token"))
 o.datatype = "string"
 o.password = true
 o.placeholder = "Enter your Telegram Bot Token"
 o.rmempty = false
 o.size = 100  -- Set field size (length of input box)
+
+o = s:option(Value, "group_id", translate("Chat ID"))
+o.datatype = "string"
+o.placeholder = "Enter your Telegram Chat ID"
+
+o = s:option(Value, "message_thread_id", translate("Message Thread ID"))
+o.datatype = "string"
+o.placeholder = "Enter your Telegram Message Thread ID"
 
 return m
